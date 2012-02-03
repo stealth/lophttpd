120503b8ad181aa163b53eb3ea464f10a2d9d771
Sebastian Krahmer <sebastian.krahmer@gmail.com>
2012-02-03 13:23:28 +0100

    One more test case, for frontend.

---
+++ tests/test7.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use IO::Socket;
+
+my $host = shift || "127.0.0.1";
+
+my $peer = new IO::Socket::INET->new(PeerAddr => $host,
+	PeerPort => 80,
+	Proto => 'tcp') or die $!;
+
+my $msg1=<<EOH;
+GET /foo HTTP/1.1\r\nConnection: keep-alive\r\nHost: $host\r\n\r
+EOH
+
+my $msg2=<<EOH;
+GET /foo/index.html HTTP/1.1\r\nConnection: keep-alive\r\n\r
+EOH
+
+
+print $peer $msg1;
+print $peer $msg2;
+
+while (1) {
+	$msg = "";
+	last if $peer->read($msg, 1024) == 0;
+	print $msg;
+}
+
+close($peer);
+
